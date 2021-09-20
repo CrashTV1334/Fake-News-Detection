d8b7fc03afa4cab11d29ac602630641e6a1bf80b
CrashTV1334 <iit2019033@iiita.ac.in>
2021-09-20 12:26:40 +0530

    added very basic layout

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var app = express();
 
 const PORT = 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res){
-    res.send("Hello");
+    res.render("index.ejs");
 });
+
+app.get("/check-news", function(req,res){
+    res.send("You are on /check-news");
+})
 
 app.listen(PORT, function(){
     console.log("Running on PORT 3000");
