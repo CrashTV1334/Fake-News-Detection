a0452424713843e9e56267b2442e0512ee9a7467
CrashTV1334 <iit2019033@iiita.ac.in>
2021-11-07 22:16:37 +0530

    Connection with backend - Done

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var passport = require("passport");
-var LocalStrategy = require("passport-local");
-var passportLocalMongoose = require("passport-local-mongoose");
+var XMLHttpRequest = require('xhr2');
+const request = require('request-promise');
+var xmlhttp = new XMLHttpRequest();
 
+var news_arr = [];
+var session_key="";
+var user_name="";
+var vote_id = "";
 
-window = {};
-
-var User = require("./models/user");
-var News = require("./models/news");
-
-var seedDB = require("./seeds");
-const { exists } = require("./models/user");
-
-
-var flag = 0;
-const PORT = 3000;
-
-const uri = "mongodb+srv://algoristy4:I68jYIrRxaijCoMv@profile-news.2cijj.mongodb.net/userProfileDatabase?retryWrites=true&w=majority";
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+var url = "https://fake-news-detector-grouproject.herokuapp.com/";
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -29,191 +18,145 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(require("express-session")({
-    secret: "This the 5th sem Algoristy mini project",
-    resave: false,
-    saveUninitialized: false
-}));
+var port = process.env.PORT || 3000;
+var newsId;
+var indx = 0;
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-passport.use(new LocalStrategy(User.authenticate()));
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        news_arr = JSON.parse(this.responseText);
+        proceedFunction()
+    }
+};
+xmlhttp.open("GET", 'https://fake-news-detector-grouproject.herokuapp.com/get/news/20', true);
+xmlhttp.send();
 
-// seedDB();
-app.use(function (req, res, next) {
-    res.locals.currentUser = req.user;
-    next();
-});
-
-var pg1 = 0;
-
-app.get("/", function(req, res){
-    res.render("index.ejs", {currPg: pg1});
-});
-
-app.get("/downvote/:newsid/:pg2", function(req, res){
-    // console.log(req.params.newsid);
-    curr_news_id = req.params.newsid;
-    curr_username = res.locals.currentUser.username;
-    curr_user_news = res.locals.currentUser.news;
-    var pg = req.params.pg2;
-
-    News.find({newsId: curr_news_id}, function(err, nws){
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(nws.length==0)
-            {
-                var newNews = {
-                    newsId: curr_news_id,
-                    fakePercentage: 0,
-                    votes: ['']
-                };
-                News.create(newNews, function(err, newlyCreated){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        News.find({newsId: curr_news_id}, function(err, nws1){
-                            if(err){
-                                console.log(err);
-                            }
-                            else{
-                                if(curr_user_news.includes(curr_news_id)===true)
-                                {
-            
-                                }
-                                else{
-                                console.log("DOWNVOTE");
-                                console.log(nws1);
-                                res.locals.currentUser.news.push(curr_news_id);
-                                nws1[0].votes.push(curr_username);
-                                nws1[0].save();
-                                res.locals.currentUser.save();
-                                }
-                                flag = 1;
-                                pg1 = pg;
-                                res.redirect("/");
-                            }
-                        });
-                    }
-                });
-            }
-            else
-            {
-                News.find({newsId: curr_news_id}, function(err, nws1){
-                    if(err){
-                        console.log(err);
-                    }
-                    else{
-                        if(curr_user_news.includes(curr_news_id)===true)
-                        {
-    
-                        }
-                        else{
-                        console.log("DOWNVOTE");
-                        console.log(nws1);
-                        res.locals.currentUser.news.push(curr_news_id);
-                        nws1[0].votes.push(curr_username);
-                        nws1[0].save();
-                        res.locals.currentUser.save();
-                        }
-                        flag = 1;
-                        pg1 = pg;
-                        res.redirect("/");
-                    }
-                });
-            }
-            
-        }
+function proceedFunction()
+{
+    app.get("/", function(req, res){
+        newsId = news_arr[indx]._id;
+        res.redirect("/"+newsId);
     });
 
-    // flag = 0;
-    // res.redirect("/");
-});
-
-app.get("/upvote/:newsid/:pg2", function(req, res){
-    // console.log(req.params.newsid);
-    curr_news_id = req.params.newsid;
-    curr_username = res.locals.currentUser.username;
-    curr_user_news = res.locals.currentUser.news;
-    var pg = req.params.pg2;
-
-    News.find({newsId: curr_news_id}, function(err, nws){
-        if(err){
-            console.log(err);
-        }
-        else{
-            News.find({newsId: curr_news_id}, function(err, nws1){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    if(curr_user_news.includes(curr_news_id)===true)
-                    {
-                        console.log("UPVOTE");
-                        console.log(nws1);
-                        res.locals.currentUser.news.remove(curr_news_id);
-                        nws1[0].votes.remove(curr_username);
-                        nws1[0].save();
-                        res.locals.currentUser.save();
-                    }
-                    flag = 0;
-                    pg1 = pg;
-                    res.redirect("/");
-                }
-            });
-        }
+    app.get("/:newsId", function(req, res){
+        res.render("index.ejs", {
+            newsId: newsId, 
+            newsData: news_arr[indx],
+            session_key: session_key,
+            user_name: user_name
+        });
     });
 
-    // flag = 1;
-    // res.redirect("/");
-});
-
-app.get("/register", function(req, res){
-    res.render("./authenticate/register.ejs");
-});
-
-app.post("/register", function(req, res){
-    // console.log(req.body);
-    User.register(new User({
-        username: req.body.username,
-        name: req.body.name,
-        state: req.body.state,
-        city: req.body.city,
-        interests: req.body.interests
-    }), req.body.password, function (err, user) {
-        if (err) {
-            console.log("error from /register POST");
-            console.log(err);
-            return res.render("register.ejs");
-        } else {
-            passport.authenticate("local")(req, res, function () {
-                flag = 0;
-                res.redirect("/");
-            });
-        }
+    app.get("/:newId/next", function(req, res){
+        indx+=1;
+        if(indx>=news_arr.length)
+            indx = 0;
+        res.redirect("/");
     });
-});
 
-app.get("/login", function (req, res) {
-    res.render("./authenticate/login.ejs");
-});
+    app.get("/:newId/previous", function(req, res){
+        indx-=1;
+        if(indx<0)
+            indx = news_arr.length-1;
+        res.redirect("/");
+    });
 
-app.post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login"
-}), function (req, res) {
+    app.get("/:newsId/downvote", function(req, res){
+        const options = {
+            method: 'POST',
+            uri: 'https://fake-news-detector-grouproject.herokuapp.com/one/news/downvote',
+            body: {
+                "vote_id" : vote_id,
+                "news_id" : newsId
+            },
+            json: true
+        }
+        request(options).then(function (response){
+            // res.send(response);
+            res.redirect("/"+newsId);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })        
+    });
 
-});
+    app.get("/:newsId/upvote", function(req, res){
+        const options = {
+            method: 'POST',
+            uri: 'https://fake-news-detector-grouproject.herokuapp.com/one/news/upvote',
+            body: {
+                "vote_id" : vote_id,
+                "news_id" : newsId
+            },
+            json: true
+        }
+        request(options).then(function (response){
+            // res.send(response);
+            res.redirect("/"+newsId);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    });
 
-app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
-});
+    app.get("/:newsId/register", function(req, res){
+        res.render("./authenticate/register.ejs", {
+            session_key: session_key
+        });
+    });
 
-app.listen(PORT, function(){
-    console.log("Running on PORT 3000");
-});
+    app.post("/:newsId/register", function(req, res){
+        // console.log(req.body);
+        const options = {
+            method: 'POST',
+            uri: 'https://fake-news-detector-grouproject.herokuapp.com/register',
+            body: req.body,
+            json: true
+        }
+        request(options).then(function (response){
+            // res.send(response);
+            res.redirect("/:newsId/login");
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    });
+
+    app.get("/:newsId/login", function (req, res) {
+        res.render("./authenticate/login.ejs", {
+            session_key: session_key
+        });
+    });
+
+    app.post("/:newsId/login", function (req, res) {
+        // console.log(req.body);
+        const options = {
+            method: 'POST',
+            uri: 'https://fake-news-detector-grouproject.herokuapp.com/login',
+            body: req.body,
+            json: true
+        }
+        request(options).then(function (response){
+            session_key = response.key;
+            user_name = response.data.name;
+            vote_id = response.data.voteId;
+            // res.send(response);
+            res.redirect("/");
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    });
+
+    app.get("/:newsId/logout", function (req, res) {
+        session_key = "";
+        user_name = "";
+        vote_id = "";
+        res.redirect("/");
+    });
+
+    app.listen(port, function(){
+        console.log("Running on PORT "+port);
+    });
+
+}
